cdddb31a7e6d7957f9146eda9ccb648c055071d6
qxinzheng <312782144.qq.com>
2019-07-31 12:00:30 +0800

    新建电话薄菜单

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { UsersComponent } from './users/users.component';
 import { TenantsComponent } from './tenants/tenants.component';
 import { RolesComponent } from 'app/roles/roles.component';
 import { ChangePasswordComponent } from './users/change-password/change-password.component';
+import { PersonsComponent } from './persons/persons.component';
 
 @NgModule({
     imports: [
@@ -20,6 +21,7 @@ import { ChangePasswordComponent } from './users/change-password/change-password
                     { path: 'users', component: UsersComponent, data: { permission: 'Pages.Users' }, canActivate: [AppRouteGuard] },
                     { path: 'roles', component: RolesComponent, data: { permission: 'Pages.Roles' }, canActivate: [AppRouteGuard] },
                     { path: 'tenants', component: TenantsComponent, data: { permission: 'Pages.Tenants' }, canActivate: [AppRouteGuard] },
+                    { path: 'phonebook', component: PersonsComponent, data: { permission: 'Pages.Person' }, canActivate: [AppRouteGuard] },
                     { path: 'about', component: AboutComponent },
                     { path: 'update-password', component: ChangePasswordComponent }
                 ]
