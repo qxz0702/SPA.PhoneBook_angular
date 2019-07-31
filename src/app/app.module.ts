cbb040f689b71310c6570f9327afcf4ea8836150
qxinzheng <312782144.qq.com>
2019-07-31 11:47:44 +0800

    添加persons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { CreateUserDialogComponent } from '@app/users/create-user/create-user-di
 import { EditUserDialogComponent } from '@app/users/edit-user/edit-user-dialog.component';
 import { ChangePasswordComponent } from './users/change-password/change-password.component';
 import { ResetPasswordDialogComponent } from './users/reset-password/reset-password.component';
+import { PersonsComponent } from './persons/persons.component';
 
 @NgModule({
   declarations: [
@@ -62,7 +63,8 @@ import { ResetPasswordDialogComponent } from './users/reset-password/reset-passw
     CreateUserDialogComponent,
     EditUserDialogComponent,
     ChangePasswordComponent,
-    ResetPasswordDialogComponent
+    ResetPasswordDialogComponent,
+    PersonsComponent
   ],
   imports: [
     CommonModule,
